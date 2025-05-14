b2af803f55478a4b9ed0a3115e643707e2034b29
yangfeng <yangfeng1235@foxmail.com>
2025-05-14 21:37:27 +0800

    跟新命令使用llvm-18以上可以使用llvm-dwarfdump --show-sources

--- cscope.bash
+++ cscope.bash
@@ -1,7 +1,7 @@
 function gen_cscopefiles()
 {
     rm temp_cscope.txt
-        find . -name "*.elf" -exec llvm-dwarfdump -all {} + | grep "DW_AT_decl_file" | awk -F '[()]' '{print $2}' | sort | uniq >> temp_cscope.txt
+		find . -name "*.elf" -exec llvm-dwarfdump --show-sources {} + | sort | uniq >> temp_cscope.txt
         sed -i '/^$/d' temp_cscope.txt
         sed -i 's/"//g' temp_cscope.txt
         cscope -i temp_cscope.txt -Rbk
